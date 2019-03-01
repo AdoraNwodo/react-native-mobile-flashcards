cba43034a29eb43776233a0293ccf244b381212c
nennenwodo <nennenwodo@gmail.com>
2019-03-01 21:57:46 +0100

    Added methoda for managing AsyncStorage Database

--- utils/data.js
+++ utils/data.js
@@ -1,5 +1,45 @@
 import { AsyncStorage } from 'react-native'
 
+export default getDecks = async() => {
+    try {
+        const keys = await AsyncStorage.getAllKeys()
+        const items = await AsyncStorage.multiGet(keys)
+        console.log("Deck received! ")
+        return items
+    } catch (error) {
+        console.log(error, "problem")
+    }
+}
+
+export default getDeck = async(key) => {
+    try {
+        const item = await AsyncStorage.getItem(key)
+        console.log("Deck received! ")
+        return item
+    } catch (error) {
+        console.log(error, "problem getting item")
+    }
+}
+
+export default removeDeck = async(key) => {
+    try {
+        const item = await AsyncStorage.removeItem(key)
+        console.log("Deck removed! ")
+        return item
+    } catch (error) {
+        console.log(error, "problem removing item")
+    }
+}
+
+export default clearAll = async(key) => {
+    try {
+        const item = await AsyncStorage.clear()
+        console.log("DB cleared")
+    } catch (error) {
+        console.log(error, "problem clearing db")
+    }
+}
+
 export default saveDeckTitle = async (title) => {
     const deckProperties = {
         title,
@@ -7,8 +47,9 @@ export default saveDeckTitle = async (title) => {
     }
 
     try {
-        await AsyncStorage.setItem(title, deckProperties)
+        const deck = await AsyncStorage.setItem(title, deckProperties)
         console.log("Deck saved! ")
+        return deck
     } catch (error) {
         console.log("Error saving deck title")
         console.log(error)
@@ -16,5 +57,16 @@ export default saveDeckTitle = async (title) => {
 }
 
 export default addCardToDeck = async (title, card) => {
+    // get the current deck properties
+    let deckProperties = await AsyncStorage.getItem(title)
+    deckProperties.questions.push(card)
 
+    try {
+        const deck = await AsyncStorage.mergeItem(title, deckProperties)
+        console.log("Deck merged! ")
+        return deck
+    } catch (error) {
+        console.log("Error merging deck title")
+        console.log(error)
+    }
 }
